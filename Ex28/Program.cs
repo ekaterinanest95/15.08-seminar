10a3a977e677c4b0b5325284f1586e676136d441
Ekaterina <katya.kinal@gmail.com>
2022-08-15 21:14:36 +0300

    Ex28

---
+++ Ex28/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите чяисло:");
+int A = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+for (int i = 1; i <= A; i++)
+{
+    product *= i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {A} = {product} ");
+
